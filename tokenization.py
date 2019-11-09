4144e18ef5da611be371b7d14a3001a1c17c9b86
Prakhar Rastogi <51193428+prakharR534@users.noreply.github.com>
2019-11-10 01:13:34 +0530

    Create tokenization.py

---
+++ tokenization.py
@@ -0,0 +1,16 @@
+# Import necessary modules
+import re
+from nltk.tokenize import sent_tokenize
+from nltk.tokenize import word_tokenize
+
+# Split scene_one into sentences: sentences
+sentences = sent_tokenize(scene_one)
+
+# Use word_tokenize to tokenize the fourth sentence: tokenized_sent
+tokenized_sent = word_tokenize(sentences[3])
+
+# Make a set of unique tokens in the entire scene: unique_tokens
+unique_tokens = set(word_tokenize(scene_one))
+
+# Print the unique tokens result
+print(unique_tokens)
